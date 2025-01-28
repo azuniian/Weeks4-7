d5f1796619605c5d515d79c144cdc786f053d5fe
Teddy Levante <levantea@sheridancollege.ca>
2025-01-28 18:14:56 -0500

    week 4 coding gym: completed challenge 3 created a world-space UI above the barrel which has text that becomes invisible when the game starts

---
+++ Assets/Scripts/Week 4/Week 4 CG/barrelUIActive.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class barrelUIActive : MonoBehaviour
+{
+    public GameObject barrelUI;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        barrelUI.SetActive(false); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
